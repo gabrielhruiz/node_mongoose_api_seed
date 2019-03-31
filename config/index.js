abcb8043f6b84d4fe42d017ae1429d655fd8724a
Gabriel Hidalgo <gabrielhruiz@gmail.com>
2019-03-31 03:07:41 +0200

    API Routes

--- config/index.js
+++ config/index.js
@@ -3,5 +3,6 @@
 * */
 const dbConfig = require('./database');
 const logger = require('./logger');
+const router = require('./router');
 
-module.exports = { dbConfig, logger };
+module.exports = { dbConfig, logger, router };
---
+++ config/router.js
@@ -0,0 +1,30 @@
+const bodyParser = require('body-parser');
+
+const auth = require('../api/controllers/authentication');
+const user = require('../api/controllers/user');
+
+const { jwtAuthenticate } = require('../api/middlewares/authentication');
+
+const API_VERSION = process.env.API_VERSION;
+
+const bodyParserJson = bodyParser.json({
+  limit: '5mb',
+});
+const bodyParserUrl = bodyParser.urlencoded({
+  limit: '5mb',
+  extended: true,
+  parameterLimit: 50000,
+});
+
+const routes = [auth];
+const generateRoutes = (app) =>
+  app.use(`/${API_VERSION}`, bodyParserUrl, bodyParserJson, routes);
+
+const jwtRoutes = [user];
+const generateJwtRoutes = (app) =>
+  app.use(`/${API_VERSION}`, bodyParserUrl, bodyParserJson, jwtAuthenticate, jwtRoutes);
+
+exports.generateAPIRoutes = (app) => {
+  generateRoutes(app);
+  generateJwtRoutes(app);
+};
