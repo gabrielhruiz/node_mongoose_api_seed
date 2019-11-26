b0e88ea118ac2980cc80872dbe0c97623753bb5e
Gabriel Hidalgo Ruiz <ghidalgo@formarketer.com>
2019-11-26 12:31:14 +0100

    enumeration

---
+++ api/enums/error.js
@@ -0,0 +1,30 @@
+exports.ERRORS = {
+  400: {
+    status: 400,
+    statusMessage: 'Bad Request',
+  },
+  401: {
+    status: 401,
+    statusMessage: 'Unauthorized',
+  },
+  403: {
+    status: 403,
+    statusMessage: 'Forbidden',
+  },
+  404: {
+    status: 404,
+    statusMessage: 'Not Found',
+  },
+  405: {
+    status: 405,
+    statusMessage: 'Method Not Allowed',
+  },
+  500: {
+    status: 500,
+    statusMessage: 'Internal Server Error',
+  },
+  501: {
+    status: 501,
+    statusMessage: 'Not Implemented',
+  }
+};
